debadc86f436bf5e1af32021bf21aeb22dc38231
chestnut1044 <wbk1620@gmail.com>
2023-05-07 20:19:19 +0900

    useref 변수관리

--- mdreact/src/App.js
+++ mdreact/src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 import UserList from './study/UserList';
 
 function App() {
-  return (
-    <UserList />
-  );
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  return <UserList users={users} />;
 }
-
 export default App;
