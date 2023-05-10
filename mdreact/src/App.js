1c80a344901acae1ae93c3e5420c20398c26fad5
chestnut1044 <wbk1620@gmail.com>
2023-05-10 20:19:19 +0900

    배열 filter 수정

--- mdreact/src/App.js
+++ mdreact/src/App.js
@@ -8,44 +8,66 @@ export default function App() {
     email: "",
   });
   const { username, email } = inputs;
+  
   const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
       email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
       username: "liz",
       email: "liz@example.com",
+      active: false,
     },
   ]);
+
   const nextId = useRef(4);
   const onChange = (e) => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
-      [name]: value
+      [name]: value,
     });
   };
+  // const onCreate = () => {
+  //   console.log(inputs)
+  //   const user = {
+  //     id : nextId.current,
+  //     username,
+  //     email
+  //   }
+  //   setUsers([...users, user]);
+  //   setInputs({
+  //     username: "",
+  //     email: "",
+  //   });
+  //   nextId.current += 1;
+  // };
+
   const onCreate = () => {
-    console.log(inputs)
     const user = {
-      id : nextId.current,
+      id: nextId.current,
       username,
-      email
-    }
-    setUsers([...users, user]);
+      email,
+    };
+    setUsers(users.concat(user));
     setInputs({
       username: "",
       email: "",
     });
     nextId.current += 1;
+  };
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
   };
   return (
     <>
@@ -55,7 +77,7 @@ export default function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
