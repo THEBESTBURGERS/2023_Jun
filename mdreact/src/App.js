2878ccf0661fd59e09efb7b163bf7e62d809678f
chestnut1044 <wbk1620@gmail.com>
2023-05-15 20:19:19 +0900

    useCallback

--- mdreact/src/App.js
+++ mdreact/src/App.js
@@ -1,74 +1,73 @@
-import React, { useRef, useState, useMemo } from 'react';
-import UserList from './study/UserList';
-import CreateUser from './study/CreateUser';
+import React, { useRef, useState, useMemo, useCallback } from "react";
+import UserList from "./study/UserList";
+import CreateUser from "./study/CreateUser";
 
 function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
 }
 
 function App() {
   const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
+    username: "",
+    email: "",
   });
   const { username, email } = inputs;
-  const onChange = e => {
+  const onChange = useCallback((e) => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
-      [name]: value
+      [name]: value,
     });
-  };
+  });
+
   const [users, setUsers] = useState([
     {
       id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
-      email
+      email,
     };
     setUsers(users.concat(user));
 
     setInputs({
-      username: '',
-      email: ''
+      username: "",
+      email: "",
     });
     nextId.current += 1;
-  };
+  });
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  };
-  const onToggle = id => {
+  const onRemove = useCallback((id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  });
+  const onToggle = useCallback((id) => {
     setUsers(
-      users.map(user =>
+      users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  };
+  });
   // const count = countActiveUsers(users);
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
