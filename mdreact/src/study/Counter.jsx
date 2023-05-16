7891267bab6f589c2f0898ce0e458b63ede60513
chestnut1044 <wbk1620@gmail.com>
2023-05-16 20:19:19 +0900

    useReducer

--- mdreact/src/study/Counter.jsx
+++ mdreact/src/study/Counter.jsx
@@ -1,22 +1,51 @@
-import React, { useState } from 'react';
+// import React, { useState } from "react";
 
-function Counter() {
-  const [number, setNumber] = useState(0)
+// function Counter() {
+//   const [number, setNumber] = useState(0);
+//   const onIncrease = () => {
+//     setNumber((prev) => prev + 1);
+//   };
+//   const onDecreaser = () => {
+//     setNumber((prev) => prev - 1);
+//   };
+//   return (
+//     <div>
+//       <h1>{number}</h1>
+//       <button onClick={onIncrease}>+1</button>
+//       <button onClick={onDecreaser}>-1</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+
+import React, {useReducer} from "react";
+
+function reducer(state, action){
+  switch (action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT' : 
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+export default function Counter() {
+  const [number, dispatch] = useReducer(reducer, 0);
   const onIncrease = () => {
-    setNumber(prev => prev + 1);
+    dispatch({type : 'INCREMENT'});
   }
-
   const onDecrease = () => {
-    setNumber(prev => prev -1);
+    dispatch({type : 'DECREMENT'});
   }
-
   return (
     <div>
       <h1>{number}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
-  );
+  )
 }
-
-export default Counter;
--- modernreact/src/Counter.jsx
+++ modernreact/src/Counter.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Counter() {
+  const [number, setNumber] = useState(0);
   const onIncrease = () => {
-    
-  }
-
+    setNumber((prev) => prev + 1);
+  };
+  const onDecreaser = () => {
+    setNumber((prev) => prev - 1);
+  };
   return (
     <div>
-      <h1>0</h1>
+      <h1>{number}</h1>
       <button>+1</button>
       <button>-1</button>
     </div>
