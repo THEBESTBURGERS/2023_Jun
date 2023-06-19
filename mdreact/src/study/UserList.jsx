6391ac29a4494d9da6c8c644db9b87a039000ff7
chestnut1044 <wbk1620@gmail.com>
2023-06-20 01:46:12 +0900

    useref 변수관리

--- mdreact/src/study/UserList.jsx
+++ mdreact/src/study/UserList.jsx
@@ -1,3 +1,40 @@
+// import React from "react";
+
+// function User({ user }) {
+//   return (
+//     <div>
+//       <b>{user.username}</b> <span>({user.email})</span>
+//     </div>
+//   );
+// }
+
+// export default function UserList() {
+//   const users = [
+//     {
+//       id: 1,
+//       username: "velopert",
+//       email: "public.velopert@gmail.com",
+//     },
+//     {
+//       id: 2,
+//       username: "tester",
+//       email: "tester@example.com",
+//     },
+//     {
+//       id: 3,
+//       username: "liz",
+//       email: "liz@example.com",
+//     },
+//   ];
+
+//   return (
+//     <div>
+//       {users.map((user) => (
+//         <User user={user} key={user.id} /> //
+//       ))}
+//     </div>
+//   );
+// }
 import React from "react";
 
 function User({ user }) {
@@ -8,29 +45,11 @@ function User({ user }) {
   );
 }
 
-export default function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
-
+export default function UserList({users}) {
   return (
     <div>
-      {users.map((user) => (
-        <User user={user} key={user.id} /> //
+      {users.map(user => (
+        <User user={user} key={user.id} />
       ))}
     </div>
   );
